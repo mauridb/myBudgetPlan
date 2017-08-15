2538c9c3382a36c3ccaccb2b8e7055bcc14f417c
ColmeSH <mauriziocontatto@gmail.com>
2017-08-15 08:43:07 +0200

    data modeling

--- budget/planning/models.py
+++ budget/planning/models.py
@@ -11,15 +11,21 @@ class Amount(models.Model):
 
 class MacroCategory(models.Model):
     name = models.CharField(max_length=256, blank=False, null=False)
+    size = models.IntegerField(blank=False, null=False, default=0)
     amount = models.ForeignKey(Amount, blank=False, null=False, default=0)
 
     def __unicode__(self):
         return self.name
 
+    # def set_size(self, n):
+    #     self.size = n
+    #     return self.size
+
 
 class Category(models.Model):
     category = models.CharField(max_length=256, blank=False, null=False)
     macroCategory = models.ForeignKey(MacroCategory, blank=False, null=False)
+    description = models.TextField(max_length=256, default='',blank=False, null=False)
 
     def __unicode__(self):
         return self.category
--- budget/planning/views.py
+++ budget/planning/views.py
@@ -2,22 +2,25 @@
 from __future__ import unicode_literals
 
 import logging
+
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 from django.shortcuts import render
-from models import MacroCategory,Amount
+from models import MacroCategory, Amount, Category
 
 
 def index(request):
     logging.info('index page')
-    list_models = MacroCategory.objects.all()
-    list_models_name = [model.name for model in list_models]
-    list_models_name.sort()
+    list_macrocategory = MacroCategory.objects.all()
+    list_subcategory = Category.objects.all()
+    list_macrocategory_name = [model.name for model in list_macrocategory]
+    list_subcategory_name = [model.category for model in list_subcategory]
+    list_macrocategory_name.sort()
     logging.debug('list models created')
     context = {
-        'macrocategory': list_models_name,
+        'list_macrocategory': list_macrocategory_name,
+        'list_subcategory': list_subcategory_name,
         'amount': Amount.objects.first(),
         'remaining': Amount.objects.first()
     }
-
 
     return render(request, 'planning/index.html', context)
